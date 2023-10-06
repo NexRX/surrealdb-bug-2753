4cf10f0101b56ed120489c33c8cd02e7b48f4a96
NexRX <callum.cunha@gmail.com>
2023-10-06 13:15:12 +0100

    feat: reproducable datetime not being considered string

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use chrono::{DateTime, Utc};
+use surrealdb::{engine::local::Mem, Surreal};
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+struct Example {
+    pub time: DateTime<Utc>,
+}
+
+#[tokio::main]
+async fn main() {
+    let client = Surreal::new::<Mem>(()).await.unwrap();
+    client.use_ns("example").use_db("example").await.unwrap();
+    let content = Example { time: Utc::now() };
+
+    let a: Example = client
+        .create(("table", "a"))
+        .content(content)
+        .await
+        .expect("Ok")
+        .expect("Some");
+
+    dbg!(&a);
+
+    let is_datetime = client
+        .query("SELECT * FROM table:a WHERE type::is::datetime(time)")
+        .await
+        .unwrap();
+
+    dbg!(is_datetime);
+
+    let is_not_datetime = client
+        .query("SELECT * FROM table:a WHERE !type::is::datetime(time)")
+        .await
+        .unwrap();
+
+    dbg!(is_not_datetime);
+}
